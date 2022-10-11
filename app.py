0459fafddd120e863da23572e1dc1feabd1d708a
garychl <garychl320@gmail.com>
2022-10-11 11:51:09 +0800

    update to use wandb

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from functools import partial
+import argparse
+
 import gradio as gr
 import numpy as np
 
@@ -7,8 +10,14 @@ from drawer.simple_draw import add_text
 DEFAULT_MODEL_PATH = "./emotion_synthesizer/learned_generators/gaus_2d/1800000-G.ckpt"
 DEFAULT_MODEL_TYPE = "gaussian"
 
-def make_meme(original_image, new_emotion, text=None):
-    model = EmotionSynthesizer(DEFAULT_MODEL_PATH, DEFAULT_MODEL_TYPE)
+def make_meme(original_image, new_emotion, text=None, wandb_artifact=None):
+    if wandb_artifact:
+        artifact_dir = artifact.download()
+        artifact_path = f"{artifact_dir}/1800000-G.ckpt"
+        model_type = artifact.metadata["model_type"]
+        model = EmotionSynthesizer(model_path=artifact_path, model_type=model_type)
+    else:
+        model = EmotionSynthesizer(DEFAULT_MODEL_PATH, DEFAULT_MODEL_TYPE)
     try:
         generated_image = model.predict(original_image, new_emotion)
     except:
@@ -27,13 +36,36 @@ def face_to_face(x):
 
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--use_wandb", action="store_true", help="Use wandb artifact", default=False)
+    args = parser.parse_args()
+    use_wandb = args.use_wandb
+
+    if use_wandb:
+        print(f"Using wandb artifact")
+        import wandb
+        run = wandb.init(
+            project="fsdl2022-emotion", 
+            job_type='use-model',
+            entity="fsdl22",
+            )
+        artifact = run.use_artifact("ganmut-model:production")
+        meme_app = partial(make_meme, wandb_artifact=artifact)
+    else:
+        print(f"Using default model: {DEFAULT_MODEL_PATH}")
+        meme_app = make_meme
+
     # TODO
     title = "MEME Manipulation Tool"
     description = "MEME Manipulation Tool is an app..."
     article = "example article"
     examples=[["example1"]]
 
-    with gr.Blocks(title=title) as demo:
+    with gr.Blocks(
+        title=title, 
+        css=".gradio-container {background-image: url('file=wallpaper.jpg');background-repeat: no-repeat; background-size: contain;}"
+        ) as demo:
         with gr.Accordion("README"):
             gr.Markdown("MEME Manipulation Tool")
 
@@ -51,8 +83,7 @@ if __name__ == "__main__":
                 image_output = gr.Image()
             image_button = gr.Button("Convert")
 
-
-        change_emotion_button.click(make_meme, inputs=[emtion_image_input, emotion_text_input, meme_text_input], outputs=emotion_image_output, )
+        change_emotion_button.click(meme_app, inputs=[emtion_image_input, emotion_text_input, meme_text_input], outputs=emotion_image_output, )
         image_button.click(face_to_face, inputs=image_input, outputs=image_output)
 
     demo.launch()
